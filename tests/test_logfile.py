53708d327558ec939ae20247797526ea7b0e2028
tjlaboss <travis@ktc-technology.com>
2021-07-23 09:53:50 -0400

    Test the dummy function.

--- tests/test_logfile.py
+++ tests/test_logfile.py
@@ -27,6 +27,29 @@ def _get_logger():
 	return test_logger
 
 
-if __name__ == '__main__':
+def _check_file(log_file, ref_file):
+	"""Check the contents of a log file, order not important."""
+	with open(log_file, 'r') as f1:
+		log_lines = f1.readlines()
+		log_lines.sort()
+	with open(ref_file, 'r') as f2:
+		ref_lines = f2.readlines()
+		ref_lines.sort()
+	errstr = "Log file {} does not match reference file {}!"
+	assert log_lines == ref_lines, errstr.format(repr(log_file), repr(ref_file))
+
+
+def test_dummy():
 	dummy_logger = _get_logger()
 	dummy_logger.info("Testing dummy logger.")
+	mlp = multilog.Processor(3, logger=dummy_logger)
+	mlp.run(dummy_f, arglist=[(2,), ('one',), (None,)])
+	results = mlp.wait()
+	results.sort()
+	dummy_logger.info("Got results: {}".format(results))
+	_check_file(TEST_FILE, "dummy.ref")
+	print("test_dummy() passed.")
+
+
+if __name__ == '__main__':
+	test_dummy()
